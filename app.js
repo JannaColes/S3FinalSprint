8fec9ad5aad9cf97c1067ce3eee4726c3b5c5c23
Rob Vatcher <135687070+RobV34@users.noreply.github.com>
2024-04-04 13:41:44 -0230

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
-const express = require('express');
-const methodOverride = require('method-override');
+// app.js
+
+const express = require("express");
 const app = express();
-const PORT = 3000;
+const bodyParser = require("body-parser");
+const path = require("path");
+const createError = require("http-errors");
+require("dotenv").config();
 
-global.DEBUG = true;
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true })); // This is important!
-app.use(methodOverride('_method')); // So is this!
+// Import route handlers
+const homeRoutes = require("./routes/home");
+const userLoginRoutes = require("./routes/userlogin");
+const adminLoginRoutes = require("./routes/adminlogin");
+const resortsRoutes = require("./routes/resorts");
 
-// Route to render the index.ejs template
-app.get('/', (req, res) => {
-    res.render('index');
-   
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Use routes
+app.use("/", homeRoutes);
+app.use("/userlogin", userLoginRoutes);
+app.use("/adminlogin", adminLoginRoutes);
+app.use("/resorts", resortsRoutes);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// Error handler
+app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Simple app running on port ${PORT}.`);
-
+  console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
