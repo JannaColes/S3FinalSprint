b99071a466c323f718453988946324315afd4a58
Amy Dalziel <amy.dalziel@keyin.com>
2024-04-09 14:35:30 -0230

    Package and package-json working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const session = require("express-session");
 const passport = require("./services/authService"); // Update the path to where your passport config is actually located
 const path = require("path");
 const createError = require("http-errors");
+const methodOverride = require('method-override');
 require("dotenv").config(); // Load environment variables from a .env file into process.env
 
 const flash = require("connect-flash");
@@ -44,14 +45,14 @@ const homeRoutes = require("./routes/home");
 const userLoginRoutes = require("./routes/userlogin");
 const adminLoginRoutes = require("./routes/adminlogin");
 const resortsRoutes = require("./routes/resorts");
-const registerRoutes = require("./routes/register");
+// const registerRoutes = require("./routes/register");
 
 const adminRouter = require('./routes/admin');
 const userRouter = require('./routes/user');
-const resortsRouter = require('./routes/resorts'); 
+// const resortsRouter = require('./routes/resorts'); 
 const apiRouter = require('./routes/api'); 
 
-app.use("/register", registerRoutes); // Add this line to use the register routes
+// app.use("/register", registerRoutes); // Add this line to use the register routes
 
 // View engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -62,11 +63,11 @@ app.use("/", homeRoutes);
 // app.use("/adminlogin", adminLoginRoutes);
 app.use("/userlogin", userLoginRoutes);
 app.use("/resorts", resortsRoutes);
-app.use("/register", registerRoutes);
+// app.use("/register", registerRoutes);
 
 app.use('/admin', adminRouter);
 app.use('/user', userRouter);
-app.use('/resorts', resortsRouter);
+// app.use('/resorts', resortsRouter);
 app.use('/api', apiRouter);
 
 // Catch 404 and forward to error handler
