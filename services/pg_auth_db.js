633b7ef475477c56c9a6ba9dd1193444e067b006
Amy Dalziel <amy.dalziel@keyin.com>
2024-04-14 14:42:33 -0230

    Final commit

--- services/pg_auth_db.js
+++ services/pg_auth_db.js
@@ -1,28 +1,18 @@
-// require("dotenv").config();
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASS,
-//   port: process.env.DB_PORT,
-// });
-
-// module.exports = pool;
+require("dotenv").config();
+const { Pool } = require("pg");
 
 global.DEBUG = true;
 
-const { Pool } = require("pg");
-
 const pool = new Pool({
-  user: 'postgres',
-  host: "localhost",
-  database: "FinalSprint-Travel",
-  password: "KeyinSem3!",
-  port: 5433,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
+
 
 if(DEBUG) console.log("Connected to Postgres...");
 
 module.exports = pool;
+
